50e1980defba5204e578529188338c5a0b782b72
RAHUL SHARMA <rahul1985xyz@gmail.com>
2026-02-04 21:41:16 +0530

    [Rahul Sharma]TableSixToNine

---
+++ src/com/gla/arrays/TableSixToNine.java
@@ -0,0 +1,20 @@
+package com.gla.arrays;
+import java.util.Scanner;
+public class TableSixToNine {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        int[] results = new int[4];
+        int index = 0;
+        for (int i = 6; i <= 9; i++) {
+            results[index] = number * i;
+            index++;
+        }
+        index = 0;
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + results[index]);
+            index++;
+        }
+    }
+}
