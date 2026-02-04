a1861424978f521fd224ff11d4bf5a472196c938
RAHUL SHARMA <rahul1985xyz@gmail.com>
2026-02-04 22:19:41 +0530

    [Rahul Sharma]MeanHeight

---
+++ src/com/gla/arrays/MeanHeight.java
@@ -0,0 +1,16 @@
+package com.gla.arrays;
+import java.util.Scanner;
+public class MeanHeight {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] heights = new double[11];
+        double sum = 0.0;
+        for (int i = 0; i < heights.length; i++) {
+            System.out.print("Enter height of player " + (i + 1) + ": ");
+            heights[i] = input.nextDouble();
+            sum += heights[i];
+        }
+        double mean = sum / heights.length;
+        System.out.println("Mean height of the football team: " + mean);
+    }
+}
