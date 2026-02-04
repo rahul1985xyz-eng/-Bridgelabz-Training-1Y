cea1bbdf0ca1a2611945147fe0a179e651508971
RAHUL SHARMA <rahul1985xyz@gmail.com>
2026-02-04 21:32:31 +0530

    [Rahul Sharma]VotingCheck

---
+++ src/com/gla/arrays/VotingCheck.java
@@ -0,0 +1,21 @@
+package com.gla.arrays;
+import java.util.Scanner;
+public class VotingCheck {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] ages = new int[10];
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = input.nextInt();
+        }
+        for (int i = 0; i < ages.length; i++) {
+            if (ages[i] < 0) {
+                System.out.println("Invalid age: " + ages[i]);
+            } else if (ages[i] >= 18) {
+                System.out.println("The student with the age " + ages[i] + " can vote");
+            } else {
+                System.out.println("The student with the age " + ages[i] + " cannot vote");
+            }
+        }
+    }
+}
