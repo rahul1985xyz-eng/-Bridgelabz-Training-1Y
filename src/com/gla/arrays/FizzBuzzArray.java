4dd08434a03e27a19eb7f74fc3318f8c6464040b
RAHUL SHARMA <rahul1985xyz@gmail.com>
2026-02-04 22:27:25 +0530

    [Rahul Sharma]FizzBuzz Array

---
+++ src/com/gla/arrays/FizzBuzzArray.java
@@ -0,0 +1,28 @@
+package com.gla.arrays;
+import java.util.Scanner;
+public class FizzBuzzArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        int number = input.nextInt();
+        if (number <= 0) {
+            System.out.println("Invalid input");
+            System.exit(0);
+        }
+        String[] results = new String[number + 1];
+        for (int i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                results[i] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                results[i] = "Fizz";
+            } else if (i % 5 == 0) {
+                results[i] = "Buzz";
+            } else {
+                results[i] = String.valueOf(i);
+            }
+        }
+        for (int i = 1; i <= number; i++) {
+            System.out.println("Position " + i + " = " + results[i]);
+        }
+    }
+}
