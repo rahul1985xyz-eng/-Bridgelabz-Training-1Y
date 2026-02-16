2ac73d399f848fd91338a5ea57af27a679eaaba5
RAHUL SHARMA <rahul1985xyz@gmail.com>
2026-02-16 23:15:18 +0530

    [Rahul Sharma]Add BankAccount

---
+++ src/com/gla/classes_objects/assignment7/BankAccount.java
@@ -0,0 +1,41 @@
+package com.gla.classes_objects.assignment7;
+
+class BankAccount {
+    private String accountHolder;
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountHolder, String accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient Balance!");
+        }
+    }
+
+    public void displayBalance() {
+        System.out.println("Current Balance: " + balance);
+    }
+}
+
+class Main2 {
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount("Rahul", "123456", 10000);
+        acc.deposit(2000);
+        acc.withdraw(5000);
+        acc.displayBalance();
+    }
+}
+
