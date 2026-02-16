4dcf5ffb66b790ce7ab1fdf061bc7de8abb9563f
RAHUL SHARMA <rahul1985xyz@gmail.com>
2026-02-16 23:30:02 +0530

    [Rahul Sharma]Add Employee

---
+++ src/com/gla/classes_objects/assignment7/Employee.java
@@ -0,0 +1,26 @@
+package com.gla.classes_objects.assignment7;
+
+class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+}
+
+class Main2 {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Rahul", 125, 50000);
+        emp.displayDetails();
+    }
+}
