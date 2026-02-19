d47d12e3ab824324a977d586013aa8d326c1818c
RAHUL SHARMA <rahul1985xyz@gmail.com>
2026-02-19 23:21:36 +0530

    [Rahul Sharma] Animal

---
+++ src/com/gla/AnimalHierarchy/Dog.java
@@ -0,0 +1,12 @@
+package com.gla.AnimalHierarchy;
+
+class Dog extends Animal {
+    Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
