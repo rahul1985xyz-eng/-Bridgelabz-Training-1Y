63f511c4bf02f08f5fa5a31e351b965906f1e699
RAHUL SHARMA <rahul1985xyz@gmail.com>
2026-02-19 23:43:48 +0530

    [Rahul Sharma] OnlineRetailOrder

---
+++ src/com/gla/OnlineRetailOrder/ShippedOrder.java
@@ -0,0 +1,15 @@
+package com.gla.OnlineRetailOrder;
+
+class ShippedOrder extends Order {
+    String trackingNumber;
+
+    ShippedOrder(int orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    String getOrderStatus() {
+        return "Order Shipped";
+    }
+}
