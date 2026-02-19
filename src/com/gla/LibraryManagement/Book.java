e4e147312ceeb3dbf63eeab18961c59032a9dfd4
RAHUL SHARMA <rahul1985xyz@gmail.com>
2026-02-19 23:40:28 +0530

    [Rahul Sharma] LibraryManagement

---
+++ src/com/gla/LibraryManagement/Book.java
@@ -0,0 +1,11 @@
+package com.gla.LibraryManagement;
+
+class Book {
+    String title;
+    int publicationYear;
+
+    Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+}
