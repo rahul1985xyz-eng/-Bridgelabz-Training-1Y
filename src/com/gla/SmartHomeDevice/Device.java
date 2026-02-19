d10d2f809b0b632c0c09f9cb94464f1fb001f6fe
RAHUL SHARMA <rahul1985xyz@gmail.com>
2026-02-19 23:53:05 +0530

    [Rahul Sharma] SmartHomeDevice

---
+++ src/com/gla/SmartHomeDevice/Device.java
@@ -0,0 +1,11 @@
+package com.gla.SmartHomeDevice;
+
+class Device {
+    String deviceId;
+    String status;
+
+    Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+}
